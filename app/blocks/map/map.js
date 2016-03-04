d245c8ab0173a5b7122579a2c96b14597978bb92
dmin <timothybernerslee@gmail.com>
2016-03-04 16:32:20 +0300

    add contacts

--- app/blocks/map/map.js
+++ app/blocks/map/map.js
@@ -1,6 +1,8 @@
 ymaps.ready(init);
 let myMap;
 let myPlacemark;
+let myMap2;
+let myPlacemark2;
 
 function init() {
   myMap = new ymaps.Map('map', {
@@ -13,4 +15,15 @@ function init() {
     hintContent: 'ВЕТКЛИНИКА'
   });
   myMap.geoObjects.add(myPlacemark);
+
+  myMap2 = new ymaps.Map('map2', {
+    center: [55.771422, 37.63711],
+    zoom: 15,
+    controls: []
+  });
+
+  myPlacemark2 = new ymaps.Placemark([55.771422, 37.63711], {
+    hintContent: 'ВЕТКЛИНИКА'
+  });
+  myMap2.geoObjects.add(myPlacemark2);
 }
